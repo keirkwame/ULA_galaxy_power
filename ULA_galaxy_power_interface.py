91e81f0982c5e4c72101ddaad4b206d39907b477
Keir Rogers <keir@utoronto.ca>
2022-02-09 11:07:01 -0500

    Bug fixes to get pipeline working

--- ULA_galaxy_power_interface.py
+++ ULA_galaxy_power_interface.py
@@ -16,9 +16,9 @@ def execute(block, config):
     b1 = block['EFT_of_LSS', 'b1']
     b2 = block['EFT_of_LSS', 'b2']
     b3 = block['EFT_of_LSS', 'b3']
-    b4 = block['EFT_of_LSS', 'b4']
-    if b4 == 'fix_to_b2':
-        b4 = cp.deepcopy(b2)
+    #b4 = block['EFT_of_LSS', 'b4']
+    #if b4 == 'fix_to_b2':
+    b4 = cp.deepcopy(b2)
     c_ct = block['EFT_of_LSS', 'c_ct']
     c_r1 = block['EFT_of_LSS', 'c_r1']
     c_r2 = block['EFT_of_LSS', 'c_r2']
@@ -29,11 +29,11 @@ def execute(block, config):
     #z_survey = block['EFT_of_LSS', 'z_survey']
     z_survey = np.array([0.32, 0.57])
 
-    omega_m_AP = block.get('EFT_of_LSS', 'omega_m_AP', default=0.31)
-    k_M = block.get('EFT_of_LSS', 'k_M', default=0.7)
-    k_min = block.get('EFT_of_LSS', 'k_min', default=0.001)
-    k_max = block.get('EFT_of_LSS', 'k_max', default=0.5)
-    nk = block.get('EFT_of_LSS', 'nk', default=50)
+    omega_m_AP = block.get('EFT_of_LSS', 'omega_m_AP') #, default=0.31)
+    k_M = block.get('EFT_of_LSS', 'k_M') #, default=0.70)
+    k_min = block.get('EFT_of_LSS', 'k_min') #, default=0.001)
+    k_max = block.get('EFT_of_LSS', 'k_max') #, default=0.5)
+    nk = block.get('EFT_of_LSS', 'nk') #, default=50)
     k = np.linspace(k_min, k_max, num=nk)
 
     # Load linear cosmology from block
@@ -58,22 +58,24 @@ def execute(block, config):
 
     #Get linear matter power spectrum
     k_power, z_power, pk = block.get_grid('matter_power_lin', 'k_h', 'z', 'p_k') #Check order #h/Mpc, (Mpc/h)^3
+    print(k_power.shape, z_power.shape, pk.shape)
     ##Check k_power values
 
     #Set-up Pybird RSD calculation
-    monopole = np.zeros((z_survey.shape[0], nk))
+    monopole = np.zeros((nk, z_survey.shape[0]))
     quadrupole = np.zeros_like(monopole)
 
     for i, z in enumerate(z_survey):
-        idx_distance = np.where(z_distance == z)[0][0]
-        idx_growth = np.where(z_growth == z)[0][0]
-        idx_power = np.where(z_power == z)[0][0]
+        print('z =', z, np.where(z_distance == z), np.where(np.absolute(z_distance - z) < 1.e-4))
+        idx_distance = np.where(np.absolute(z_distance - z) < 1.e-4)[0][0]
+        idx_growth = np.where(np.absolute(z_growth - z) < 1.e-4)[0][0]
+        idx_power = np.where(np.absolute(z_power - z) < 1.e-4)[0][0]
 
         common = pyb.Common(optiresum=True)
         nonlinear = pyb.NonLinear(load=True, save=True, co=common)
         resum = pyb.Resum(co=common)
         projection = pyb.Projection(k, omega_m_AP, z, co=common)
-        bird = pyb.Bird(k_power, pk[idx_power], f[idx_growth, 0], d_a[idx_distance],
+        bird = pyb.Bird(k_power, pk[:, idx_power], f[0, idx_growth], d_a[idx_distance],
                         h_z[idx_distance] / h_z[idx_distance_0], z, which='full', co=common)
 
         #Do RSD calculation
@@ -86,12 +88,13 @@ def execute(block, config):
 
         #Add stochastic terms
         stochastic_term = c_e1 + (c_e2 * ((k / k_M) ** 2.))
-        monopole[i, :] = bird.fullPs[0] + stochastic_term
-        quadrupole[i, :] = bird.fullPs[1] + (4. * f[idx_growth, 0] * stochastic_term / 15.)
+        monopole[:, i] = bird.fullPs[0] + stochastic_term
+        quadrupole[:, i] = bird.fullPs[1] + (4. * f[0, idx_growth] * stochastic_term / 15.)
 
     #Save galaxy multipoles to block
-    block.put_grids('EFT_of_LSS', 'k_multipoles', k, 'z_multipoles', z_survey, 'monopole', monopole, 'quadrupole',
-                    quadrupole)
+    block.put_grid('EFT_of_LSS', 'k_multipoles', k, 'z_multipoles', z_survey, 'monopole', monopole) #, 'quadrupole',
+    #                quadrupole)
+    block.put('EFT_of_LSS', 'quadrupole', quadrupole)
 
     return 0
 
